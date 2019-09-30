003cfc4d0c285e630adbdb5f47760857416a6f17
Vladimir <troflyaninvv@gmail.com>
2019-09-30 14:54:38 +0300

    add _merge_log_files method

--- logconcat.py
+++ logconcat.py
@@ -5,8 +5,10 @@ import re
 from typing import List
 
 CONFIG_PATH = 'config.ini'
-DEFAULT_LOGS_PATH = '/var/log/ofelia/logs/'
-DEFAULT_SAVE_PATH = '/var/log/ofelia/'
+DEFAULTS = {
+    'logs_path': '/var/log/ofelia/logs/',
+    'save_path': '/var/log/ofelia/',
+}
 
 
 class ConfigNotFoundError(Exception):
@@ -15,11 +17,12 @@ class ConfigNotFoundError(Exception):
 
 class LogConcat:
 
-    required_fields = ['stdout_pattern', 'stderr_pattern']
+    required_fields = ['stdout_pattern', 'stderr_pattern', 'chunk']
 
     def __init__(self):
         self._stdout_pattern = None
         self._stderr_pattern = None
+        self.chunk_size = None
         self.extra = {}
 
         self._parse_config()
@@ -49,18 +52,18 @@ class LogConcat:
         try:
             self.stdout_pattern = config['main']['stdout_pattern']
             self.stderr_pattern = config['main']['stderr_pattern']
+            self.chunk_size = config.getint('main', 'chunk')
         except KeyError:
             raise ValueError(f'{self.required_fields} field must be in {CONFIG_PATH} section main')
 
-        if config.has_section('extra'):
-            for key in config['extra']:
-                self.extra[key] = config['extra'][key]
+        for key in DEFAULTS:
+            self.extra[key] = config.get('extra', key, fallback=None) or DEFAULTS[key]
 
     def _get_files_list_by_pattern(self, pattern):
         files = []
-        root_path = self.extra.get('logs_path') or DEFAULT_LOGS_PATH
-        for f in os.listdir(root_path):
-            f_path = os.path.join(root_path, f)
+
+        for f in os.listdir(self.extra['logs_path']):
+            f_path = os.path.join(self.extra['logs_path'], f)
             if os.path.isfile(f_path) and re.match(pattern, f):
                 files.append(f)
 
@@ -70,8 +73,37 @@ class LogConcat:
     def _sort_files_by_modification_time(files: List[str]):
         return sorted(files, key=lambda f: os.path.getmtime(f))
 
-    def _merge_log_files(self):
+    def _merge_log_files(self, pattern, log_name: str):
+        if not os.path.exists(self.extra['save_path']):
+            os.mkdir(self.extra['save_path'])
+        files = self._get_files_list_by_pattern(pattern)
+
+        with open(self.extra['save_path'], 'a+b') as f_out:
+            for batch in self._chunks(files, self.chunk_size):
+                f_out.write(self._read_batch_of_files(batch))
+
+    def _read_batch_of_files(self, files_batch: List[str]) -> bytes:
+        buf = b''
+        for name in files_batch:
+            with open(os.path.join(self.extra['logs_path'], name), 'rb') as f:
+                buf += f.read()
+        return buf
+
+    def _remove_batch_of_files(self, files_batch: List[str]):
         pass
+
+    @staticmethod
+    def _chunks(r: List, size: int):
+        """
+        Splits an iterable into chunks of size.
+        """
+        offset = 0
+        while 1:
+            res = r[offset: offset + size]
+            if not res:
+                raise StopIteration
+            yield res
+            offset += size
 
 
 def main():
