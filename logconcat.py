b7630e392e38d1b4e61058a3f737612c7c45e174
Vladimir <troflyaninvv@gmail.com>
2019-09-30 14:02:30 +0300

    add file sorting by modification time

--- logconcat.py
+++ logconcat.py
@@ -2,8 +2,7 @@ import configparser
 import logging
 import os
 import re
-from os import listdir
-from os.path import isfile, join
+from typing import List
 
 CONFIG_PATH = 'config.ini'
 DEFAULT_LOGS_PATH = '/var/log/ofelia/logs/'
@@ -16,12 +15,11 @@ class ConfigNotFoundError(Exception):
 
 class LogConcat:
 
-    required_fields = ['stdout_pattern', 'stderr_pattern', 'time_mask']
+    required_fields = ['stdout_pattern', 'stderr_pattern']
 
     def __init__(self):
         self._stdout_pattern = None
         self._stderr_pattern = None
-        self.time_mask = None
         self.extra = {}
 
         self._parse_config()
@@ -51,7 +49,6 @@ class LogConcat:
         try:
             self.stdout_pattern = config['main']['stdout_pattern']
             self.stderr_pattern = config['main']['stderr_pattern']
-            self.time_mask = config['main']['time_mask']
         except KeyError:
             raise ValueError(f'{self.required_fields} field must be in {CONFIG_PATH} section main')
 
@@ -62,10 +59,19 @@ class LogConcat:
     def _get_files_list_by_pattern(self, pattern):
         files = []
         root_path = self.extra.get('logs_path') or DEFAULT_LOGS_PATH
-        for f in listdir(root_path):
-            f_path = join(root_path, f)
-            if isfile(f_path) and re.match(pattern, f):
+        for f in os.listdir(root_path):
+            f_path = os.path.join(root_path, f)
+            if os.path.isfile(f_path) and re.match(pattern, f):
                 files.append(f)
+
+        return self._sort_files_by_modification_time(files)
+
+    @staticmethod
+    def _sort_files_by_modification_time(files: List[str]):
+        return sorted(files, key=lambda f: os.path.getmtime(f))
+
+    def _merge_log_files(self):
+        pass
 
 
 def main():
